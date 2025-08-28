0fd09f778a2e8a96bd8108f3876ad270fe7e7718
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-08-28 19:44:43 +0300

    Add log out button to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import "./App.css";
 import { useReducer } from "react";
 import MessageForm from "./components/message-form/message-form";
 import Messages from "./components/messages/messages";
 import MessagesContext, { initialMessages, messagesReducer } from "./data/messages-context";
-
-import "./App.css";
+import Button from "./components/button/button";
 
 function App() {
   const [messages, dispatch] = useReducer(messagesReducer, initialMessages);
@@ -11,6 +11,7 @@ function App() {
   return <>
     <header className="page-header">
       <h1 className="page-title">Online Chat</h1>
+      <Button className="page-header-logout">Log out</Button>
     </header>
     <main className="page-main">
       <MessagesContext.Provider value={[messages, dispatch]}>
