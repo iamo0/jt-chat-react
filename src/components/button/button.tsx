8ee8a82b57c23484191dfbf340494af4a3c66989
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-08-28 19:44:58 +0300

    Add disabled state for buttons

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,13 +3,19 @@ import type { PropsWithChildren } from "react";
 
 interface ButtonProps extends PropsWithChildren {
   className?: string,
+  disabled?: boolean,
   type?: "button" | "submit" | "reset",
 }
 
 export default function Button({
   children,
   className = "",
+  disabled = false,
   type = "button"
 }: ButtonProps) {
-  return <button className={`my-button ${className}`} type={type}>{children}</button>
+  return <button
+    className={`my-button ${className}`}
+    disabled={disabled}
+    type={type}
+  >{children}</button>
 };
