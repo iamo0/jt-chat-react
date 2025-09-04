c5f728f134e292e98ef9adc029eb4008d93092c6
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-09-05 00:16:50 +0300

    Add delete message action

--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -1,22 +1,29 @@
 import "./message.css";
 import ButtonWithConfirmation from "../button/button-with-confirmation";
+import { useDispatch } from "react-redux";
+import { deleteMessage } from "../../data/message-reducer";
 
 interface MessageProps {
   message: {
     text: string,
     author: string,
     date: number,
-  }
+  },
+  i: number,
 }
 
-export default function Message({ message }: MessageProps) {
+export default function Message({ message, i }: MessageProps) {
+  const dispatch = useDispatch();
+
   return <article className="message">
     <p className="message-text">{message.text}</p>
     <p className="message-details">{message.author}, {new Date(message.date).toLocaleString("ru-RU")}</p>
     <ButtonWithConfirmation
       className="message-delete"
       confirmationMessage="Вы действительно хотите удалить это сообщение?"
-      onClick={() => { alert("Cообщение удалено") }}
+      onClick={() => { dispatch(deleteMessage({
+        id: i,
+      })) }}
     >Удалить</ButtonWithConfirmation>
   </article>;
 }
--- src/components/messages/messages.tsx
+++ src/components/messages/messages.tsx
@@ -14,7 +14,7 @@ export default function Messages({ className }: MessagesProps) {
   return <>
     <Counter />
     <section className={`${className} messages`}>
-      {messages.map((m, i) => <Message message={m} key={`message-${i}`} />)}
+      {messages.map((m, i) => <Message message={m} i={i} key={`message-${i}`} />)}
     </section>
   </>;
 }
