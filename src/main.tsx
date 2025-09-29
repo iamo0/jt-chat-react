41245a62048a56f39c4612175567d725fde9afc3
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-09-29 21:22:58 +0300

    Add lazy loading to routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,28 @@
-import { StrictMode } from 'react'
+import { lazy, StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import AuthPage from './pages/AuthPage.tsx'
-import BlogLayout from './pages/blog/blog-layout.tsx'
-import BlogNewsItem from './pages/blog/blog-news-item.tsx'
-import BlogNewsIndex from './pages/blog/blog-news-index.tsx'
+
+import './index.css'
+
+const LazyBlogLayout = lazy(() => import("./pages/blog/blog-layout.tsx"))
+const LazyBlogNewsIndex = lazy(() => import("./pages/blog/blog-news-index.tsx"))
+const LazyBlogNewsItem = lazy(() => import("./pages/blog/blog-news-item.tsx"))
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/signup" element={<AuthPage />} />
-        <Route path="/blog" element={<BlogLayout />}>
-          <Route index element={<BlogNewsIndex />} />
-          <Route path="/blog/:newsId" element={<BlogNewsItem />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<p>Идет загрузка...</p>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/signup" element={<AuthPage />} />
+          <Route path="/blog" element={<LazyBlogLayout />}>
+            <Route index element={<LazyBlogNewsIndex />} />
+            <Route path="/blog/:newsId" element={<LazyBlogNewsItem />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   </StrictMode>,
 );
