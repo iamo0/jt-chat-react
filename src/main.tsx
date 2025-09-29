927bc84b0cefbbd40057e141ffcc6bc404fa3f8e
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-09-29 23:33:01 +0300

    Add page 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
+import './index.css'
+
 import { lazy, StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import AuthPage from './pages/AuthPage.tsx'
 
-import './index.css'
-
 const LazyBlogLayout = lazy(() => import("./pages/blog/blog-layout.tsx"))
 const LazyBlogNewsIndex = lazy(() => import("./pages/blog/blog-news-index.tsx"))
 const LazyBlogNewsItem = lazy(() => import("./pages/blog/blog-news-item.tsx"))
+const LazyPage404 = lazy(() => import("./pages/page-404/page-404.tsx"))
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')!).render(
             <Route index element={<LazyBlogNewsIndex />} />
             <Route path="/blog/:newsId" element={<LazyBlogNewsItem />} />
           </Route>
+          <Route path="*" element={<LazyPage404 />} />
         </Routes>
       </BrowserRouter>
     </Suspense>
---
+++ src/pages/page-404/page-404.tsx
@@ -0,0 +1,6 @@
+export default function Page404() {
+  return <div style={{ padding: 20 }}>
+    <h1>404</h1>
+    <p>Страница не найдена</p>
+  </div>;
+}
